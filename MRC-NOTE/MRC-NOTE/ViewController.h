859b8bc33935e85a5158bc65ef4cb80f3f922887
XuJian <xcoderj@gmail.com>
2017-02-12 21:15:46 +0800

    MRC

---
+++ MRC-NOTE/MRC-NOTE/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MRC-NOTE
+//
+//  Created by xujian on 2017/2/12.
+//  Copyright © 2017年 xujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MRC-NOTE/MRC-NOTE/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MRC-NOTE
+//
+//  Created by xujian on 2017/2/12.
+//  Copyright © 2017年 xujian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
